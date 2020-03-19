c700b738af1921127438167bdd363f66acbd3fb8
kirilcube <kiril302005@gmail.com>
2020-03-19 15:30:19 +0200

    g

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
   const p = new Promise(() => {
-    fetch("https://faceit-get-data.herokuapp.com//");
+    fetch("https://faceit-get-data.herokuapp.com/");
   }).then(() => {
     debugger;
   });
@@ -10,7 +10,7 @@ var xhr = new XMLHttpRequest();
 
 var body = JSON.stringify("Mirage");
 
-xhr.open("POST", "/submit", true);
+xhr.open("POST", "https://faceit-get-data.herokuapp.com/", true);
 
 xhr.onreadystatechange = aadsds => {
   debugger;
