8b2c7a827bf3841d65c49b2083b113f77de1b126
kirilcube <kiril302005@gmail.com>
2020-03-19 15:04:44 +0200

    change urk

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 window.addEventListener("load", () => {
   const p = new Promise(() => {
-    fetch("https://intense-escarpment-96361.herokuapp.com/");
+    fetch("https://faceit-get-data.herokuapp.com//");
   }).then(() => {
     debugger;
   });
 });
+
+var xhr = new XMLHttpRequest();
+
+var body = JSON.stringify("Mirage");
+
+xhr.open("POST", "/submit", true);
+
+xhr.onreadystatechange = aadsds => {
+  debugger;
+};
+
+xhr.send(body);
